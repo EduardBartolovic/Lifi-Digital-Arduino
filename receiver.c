cc1eb06a541dce4f4ff148aa43a2516ab976561d
Eduard B <Eduard.Bartolovic@posteo.de>
2019-07-01 16:54:16 +0200

    Update receiver.c

--- receiver.c
+++ receiver.c
@@ -1,61 +1,73 @@
-// code for receiving using the LDR
-
 unsigned long StartTime = millis();
 
-unsigned long Value; unsigned long EndTime;
+unsigned long Value; 
+
+unsigned long EndTime;
 
 int Message;
 
+int counter = 0;
+
+byte group[7] = {0,0,0,0,0,0,0};
+
+int weights[7] = {1,2,4,8,16,32,64};
+
 boolean StartedCounting = false; //this is used to determine if the counting has started
 
-void setup() { Serial.begin(9600); }
+void setup() {
+  Serial.begin(9600);
+}
 
 void loop() {
+  int sensorValue = analogRead(A0); //read the sensor value
 
-int sensorValue = analogRead(A0); //read the sensor value
+  delay(1); // to stabilize
 
-delay(1); // to stabilize
+  //Serial.println(sensorValue); // this can be used to determine the threshold in your lighting conditions
 
-// Serial.println(sensorValue); // this can be used to determine the threshold in your lighting conditions
+  if (StartedCounting == false && sensorValue > 900) { //if the counting has not yet started and the light is on
 
-if (StartedCounting==false && sensorValue>850){ //if the counting has not yet started and the light is on
+    StartTime = millis(); // This saves the current time in milli seconds
 
-StartTime = millis(); // This saves the current time in milli seconds
+    StartedCounting = true;
+  }
 
-StartedCounting= true; }
+  if (StartedCounting == true && sensorValue < 900) { // if the counting has started and the light is off
 
-if (StartedCounting== true && sensorValue<850){ // if the counting has started and the light is off
+    Value = millis() - StartTime; // subtract the starting time from the current time to get message time 
+    
+    Message = Value/8; // Message 
 
-Value = millis() - StartTime; // subtract the starting time from the current time to get message time Message= Value/1000;
+    StartedCounting = false;
 
-StartedCounting= false;
-
-Serial.print ("Message: ");
-
-if (Message == 1){ // change these to your own messages. the sender should also have this list so they know what they send.
-
-Serial.println("hi"); }
-
-else if (Message == 2){
-
-Serial.println("wazzup?"); }
-
-else if (Message == 3){
-
-Serial.println("HELP"); }
-
-else if (Message == 4){
-
-Serial.println("top secret"); }
-
-else if (Message == 5){
-
-Serial.println("Penguins"); } '
-
-else if (Message == 6){
-
-Serial.println("LI-FI is the future"); }
-
-else if (Message == 7){
-
-Serial.println("#notreallythough"); } } }
+    if (Message == 1) { 
+      group[counter] = 0;
+      //Serial.println("0");
+      counter += 1;
+    } else if (Message == 2) {
+      group[counter] = 1;
+      counter += 1;
+      //Serial.println("1");
+    } else {
+      group[0] = 0;
+      group[1] = 0;
+      group[2] = 0;
+      group[3] = 0;
+      group[4] = 0;
+      group[5] = 0;
+      group[6] = 0;
+      group[7] = 0;
+      counter = 0;
+    }
+  }
+  
+  if(counter>6){
+    int chara = 0;
+    for(int i=0;i<8;i++){
+      chara += group[i]*weights[i];
+    }
+    char final = chara;
+    Serial.print(final);
+    counter = 0;
+  }
+}
